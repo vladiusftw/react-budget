975ce61f03bd56ea145eee36b4038d1adf0adc84
Abdul Rahman Al Sbeinaty <61135058+vladiusftw@users.noreply.github.com>
2023-08-17 17:35:30 +0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import ExpenseTotal from '@/components/ExpenseTotal';
 import ExpenseList from '@/components/ExpenseList';
 import AllocationForm from '@/components/AllocationForm';
 
-const inter = Inter({ subsets: ['latin'] });
+// const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
